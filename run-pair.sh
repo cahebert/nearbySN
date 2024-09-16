3bd03bdac75cc2167e4e56df2c4ed6afa474ddce
cahebert <claireahebert@me.com>
2024-09-16 12:52:55 -0400

    run script update

--- run-pair.sh
+++ run-pair.sh
@@ -1,10 +1,25 @@
-#!/usr/bin/bash
+sigma2=$3
+sigma4=$2
+sigma6=$1
 
-instcat=$1
-outdir=$2
-#N=$2
+maxv=100000
 
-galsim custom-imsim-config.yaml \
-   input.instance_catalog.file_name=$instcat \
-   output.dir=${outdir} \
-   output.truth.dir=${outdir}
+~/Documents/git/hotpants/hotpants \
+-tmplim ~/Documents/catchingSN/eimage_00701782-0-r-R22_S11-det094.fits \
+-inim 	~/Documents/catchingSN/00699127-0-r-R22_S11-det094_dithered_projected00701782.fits \
+-outim 	~/Documents/catchingSN/diaimg_i00699127d_t00701782_medium.fits \
+-tu	$maxv \
+-iu	$maxv \
+-gd  	2200 2700 1500 2000 \
+-ng 	3 6 $sigma6 4 $sigma4 2 $sigma2
+
+
+~/Documents/git/hotpants/hotpants \
+-tmplim     ~/Documents/catchingSN/eimage_00701782-0-r-R22_S11-det094.fits \
+-inim       ~/Documents/catchingSN/00699127-0-r-R22_S11-det094_dithered_projected00701782.fits \
+-outim      ~/Documents/catchingSN/diaimg_i00699127d_t00701782_dense.fits \
+-tu	    $maxv \
+-iu	    $maxv \
+-gd         500 1000 1000 1500 \
+-ng         3 6 $sigma6 4 $sigma4 2 $sigma2
+
