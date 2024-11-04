4fbddcc7090bf29d0dbae12d638ef945f25cb76e
cahebert <claireahebert@me.com>
2024-11-04 15:15:07 -0500

    nside query and randomize output pos

--- get_instcat_pair.py
+++ get_instcat_pair.py
@@ -3,6 +3,19 @@ import rubin_sim_util
 import opsim_util
 import plotting
 
+def get_healpix_info(ra, dec, neighbors=True, nside=4096, nest=True):
+    import healpy
+    # get ra/dec central to that healpix, will be what we query opsim for
+    healpix = healpy.ang2pix(nside, theta=ra, phi=dec, lonlat=True, nest=nest)
+    ra, dec = healpy.pix2ang(nside, healpix, lonlat=True, nest=nest)
+
+    if neighbors:
+        healpix = [healpix]
+        healpix += list(healpy.get_all_neighbours(nside, healpix[0], nest=True))
+
+    return ra, dec, healpix
+
+
 def get_args():
     from argparse import ArgumentParser
     parser = ArgumentParser()
@@ -16,7 +29,7 @@ def get_args():
     parser.add_argument('--window', default=0.12, type=float)
     parser.add_argument('--dust', default=True, action='store_false')
     parser.add_argument('--noplot', default=False, action='store_true')
-    parser.add_argument('--randomize', default=False, action='store_true')
+    parser.add_argument('--keeppos', default=False, action='store_true')
     parser.add_argument('--magcut', default=None)
     parser.add_argument('--catdir', default='./instcats/')
     parser.add_argument('--pair', default=None)
@@ -33,7 +46,7 @@ if __name__ == '__main__':
         from dl import authClient as ac
         token = ac.login(input("Enter user name: "),getpass("Enter password: "))
         ac.whoAmI()
-    
+
     rng = np.random.default_rng(args.seed)
 
     if not args.noplot:
@@ -44,31 +57,24 @@ if __name__ == '__main__':
 
     # convert to celestial coords
     ra, dec = opsim_util.get_celestial_coord(args.l, args.b)
-
-    import healpy
-    healpix = healpy.ang2pix(256, theta=args.l, phi=args.b, lonlat=True)
+    ra, dec, healpix = get_healpix_info(ra, dec)
 
     # query baseline for visits near that spot, choose pair close together and with few day lag
     visit_1, visit_2 = opsim_util.get_opsim_visit_pair(ra, dec, args.db)
-    
+
     # make header for both cats
     header_1 = opsim_util.assemble_instcat_header(visit_1, seed=int(rng.uniform()*1000), exptime_=args.exptime)
     header_2 = opsim_util.assemble_instcat_header(visit_2, seed=int(rng.uniform()*1000), exptime_=args.exptime)
-    
+
     # artificially tweak ra/dec to be separated by just 10 arcsec (=.00278 deg) dither
-    new_ra = np.mean([header_1['rightascension'], header_2['rightascension']])
-    new_dec = np.mean([header_1['declination'], header_2['declination']])
+    delta_ra = 0.00278
 
-    header_1['rightascension'] = new_ra
-    header_1['declination'] = new_dec
-    header_2['rightascension'] = new_ra + (1/360) * np.sqrt(2)
-    header_2['declination'] = new_dec + (1/360) * np.sqrt(2)
+    header_1['rightascension'] = ra
+    header_1['declination'] = dec
+    header_2['rightascension'] = ra + delta_ra
+    header_2['declination'] = dec + delta_ra
 
-    # # query rubin_sim for stars that fall within 1 + sqrt(2) * CCD area of the observation center
-    # ra_min, ra_max = new_ra - args.window * (1 + np.sqrt(2)), new_ra + args.window * (1 + np.sqrt(2))
-    # dec_min, dec_max = new_dec - args.window * (1 + np.sqrt(2)), new_dec + args.window * (1 + np.sqrt(2))
-    
-    out = rubin_sim_util.query_data_lab(healpix, args.N, magcut=args.magcut, save=False)    
+    out = rubin_sim_util.query_data_lab(healpix, args.N, indextype='nest4096', magcut=args.magcut, save=False)
 
     # dump into .txt with the instcat headers above
     if args.pair is not None:
@@ -77,24 +83,18 @@ if __name__ == '__main__':
         pair = str(int(np.random.uniform() * 1000))
 
     np.savetxt(args.catdir+f'imags_p{pair}.txt', out['imag'].to_numpy())
-    
-    rubin_sim_util.make_inst_cat(out, 
+
+    rubin_sim_util.make_inst_cat(out,
                                  header=header_1,
-                                 catdir=args.catdir, 
-                                 dust=args.dust, 
+                                 catdir=args.catdir,
+                                 dust=args.dust,
                                  pair=pair,
-                                 randomize=args.randomize)
+                                 randomize=~args.keeppos)
 
-    rubin_sim_util.make_inst_cat(out, 
-                                 header=header_2, 
-                                 catdir=args.catdir, 
-                                 dust=args.dust, 
-                                 pair=pair
-                                 randomize=args.randomize,
+    rubin_sim_util.make_inst_cat(out,
+                                 header=header_2,
+                                 catdir=args.catdir,
+                                 dust=args.dust,
+                                 pair=pair,
+                                 randomize=~args.keeppos,
                                  write_stars=False)
-
-
-    # launch imsim for both
-    # run alignment
-    # check header for seeing
-    # use seeing to run hotpants (det which is template vs img, and size of gauss)
--- rubin_sim_util.py
+++ rubin_sim_util.py
@@ -1,20 +1,24 @@
 import numpy as np
 
-def query_data_lab(healpix, N, expid=None, magcut=None, save=False):
+def query_data_lab(index, N, indextype='nest4096',expid=None, magcut=None, save=False):
     """Fetch TRILEGAL positions and gmag for a given ra/dec region."""
     from dl import queryClient as qc
-    magstring = f'AND imag<{magcut}' if magcut is not None else ''
 
+    magstring = f'AND imag<{magcut}' if magcut is not None else ''
+    if indextype == 'nest4096':
+        index = ' in ('+', '.join([str(i) for i in index])+')'
+    else:
+        index = f'={index}'
     query = \
        """SELECT ra, dec, av, imag, gc, logte, logg, logl, mu0, z
           FROM lsst_sim.simdr2
-          WHERE (ring256={} AND label!=9 {})
+          WHERE ({}{} AND label!=9 {})
           LIMIT {}
-          """.format(healpix, magstring, N)
+          """.format(indextype, index, magstring, N)
 
     print("Submitting request:\n")
     print(query)
-    
+
     result = qc.query(sql=query,
                       fmt='pandas',
                       async_=True,
@@ -23,12 +27,41 @@ def query_data_lab(healpix, N, expid=None, magcut=None, save=False):
                       verbose=1)
 
     print(f"Request complete: catalog length {len(result)}")
-    
+
     if save:
         np.savetxt(f'{args.catdir}/gc_values_{expid}.txt', np.array(result['gc']))
-               
+
     return result
 
+def random_points_within_polygon(ra, dec, n_points):
+    """Function to generate random points within RA/Dec area"""
+    from shapely.geometry import Polygon, Point
+
+    points = []
+    points.append((ra[np.argmin(ra)], dec[np.argmin(ra)]))
+    points.append((ra[np.argmin(dec)], dec[np.argmin(dec)]))
+    points.append((ra[np.argmax(ra)], dec[np.argmax(ra)]))
+    points.append((ra[np.argmax(dec)], dec[np.argmax(dec)]))
+    polygon = Polygon(points)
+
+    min_x, min_y, max_x, max_y = polygon.bounds
+    positions = []
+
+    while len(positions) < n_points:
+        random_point = Point(np.random.uniform(min_x, max_x), np.random.uniform(min_y, max_y))
+        if polygon.contains(random_point):
+            positions.append(random_point)
+
+    return np.array([[point.x, point.y] for point in positions])
+
+def randomize_positions(df):
+    """Randomize positions of stars in dataframe."""
+    ra = df['ra'].to_numpy()
+    dec = df['dec'].to_numpy()
+    new_positions = random_points_within_polygon(ra, dec, len(ra))
+    df['ra'] = new_positions[:,0]
+    df['dec'] = new_positions[:,1]
+    return df
 
 def save_summary(expid, df, header, args):
     # expid, ra, dec, gal l, gal b, n_stars, n_stars w g>24.5, p_thin, p_thick, p_halo, p_bulge
@@ -45,14 +78,14 @@ def save_summary(expid, df, header, args):
         len(df[df['gc']==3]) / len(df),
         len(df[df['gc']==4]) / len(df),
     ]
-    
+
     summary_f = f'{args.catdir}/pointing_summaries.txt'
     with open(summary_f, 'a') as f:
         f.write(' '.join([str(s) for s in summary]) + '\n')
 
 def get_sed_info(star, etoiles, index, sed_path='/Users/clairealice/Documents/share/rubin_sim_data/sims_sed_library/'):
     """Returns magnorm and path to file for SED of given star.
-    
+
     star is dict or row of dataframe."""
     import galsim
     import astropy.units as u
@@ -72,13 +105,12 @@ def get_sed_info(star, etoiles, index, sed_path='/Users/clairealice/Documents/sh
     flambda0 = hnu0 * sed(wl.value) * (1./u.nm/u.cm**2/u.s)
     fnu0 = flambda0 * wl**2 / astropy.constants.c
     magnorm = fnu0.to_value(u.ABmag)
-    
-    return magnorm, fname    
+
+    return magnorm, fname
 
 
 def get_sed_info(star, etoiles, index, sed_path='/Users/clairealice/Documents/share/rubin_sim_data/sims_sed_library/'):
     """Returns magnorm and path to file for SED of given star.
-    
     star is dict or row of dataframe."""
     import galsim
     import astropy.units as u
@@ -98,14 +130,14 @@ def get_sed_info(star, etoiles, index, sed_path='/Users/clairealice/Documents/sh
     flambda0 = hnu0 * sed(wl.value) * (1./u.nm/u.cm**2/u.s)
     fnu0 = flambda0 * wl**2 / astropy.constants.c
     magnorm = fnu0.to_value(u.ABmag)
-    
-    return magnorm, fname    
 
-def make_inst_cat(df, header, catdir,  
-                  magcut=True, randomize=True, 
+    return magnorm, fname
+
+def make_inst_cat(df, header, catdir,
+                  magcut=True, randomize=True,
                   dust=True, pair=None, write_stars=True):
     """Make a instance catalog based on TRILEGAL dataframe.
-    
+
     Dataframe columns must include ra, dec, av, and gmag."""
     import pylumiere
     print("Assembling catalog...\n")
@@ -115,9 +147,6 @@ def make_inst_cat(df, header, catdir,
         pair = '_p' + str(pair)
     else:
         pair = ''
-
-    # for now, same SED file for everyone
-    # sedpath = 'starSED/phoSimMLT/lte035-4.5-1.0a+0.4.BT-Settl.spec.gz'
 
     fname = f'instcat_trilegal{pair}_{expid}{"" if dust else "_nodust"}.txt'
     fname = catdir + fname
@@ -134,24 +163,20 @@ def make_inst_cat(df, header, catdir,
 
     except FileNotFoundError:
         instCat = open(fname, 'a')
-   
+
     # write header section
     for key, val in header.items():
         instCat.write(f'{key} {val}\n')
 
     if write_stars:
+        if randomize:
+            df = randomize_positions(df)
         etoiles = pylumiere.Stellib('BTSettl', rv=3.1, dustmodel='O94')
         # now write objects
         for row in df.index:
             df_row = df.loc[row]
-            if randomize:
-                ra, dec = np.random.uniform(low=-0.1, high=0.1, size=2)
-                ra += header['rightascension']
-                dec += header['declination']
-            else:
-                ra, dec = df_row['ra'], df_row['dec']
             magnorm, sedpath = get_sed_info(df_row, etoiles, row)
-            to_write = f"object {row} {ra} {dec} {magnorm} {sedpath} " + \
+            to_write = f"object {row} {df_row['ra']} {df_row['dec']} {magnorm} {sedpath} " + \
                     f"0 0 0 0 0 0 point none CCM " + \
                     f"{df_row['av'] if dust else 0} 3.1\n"
             instCat.write(to_write)
@@ -161,65 +186,62 @@ def make_inst_cat(df, header, catdir,
     print(f"Successfully written catalog of length {len(df)} to file {fname}.\n")
     return fname
 
-def main(args):
-    import opsim_util
-    ra, dec = opsim_util.get_celestial_coord(args.l, args.b)
-    
-    import healpy
-    healpix = healpy.ang2pix(256, theta=args.l, phi=args.b, lonlat=True)
-    # # window argument should be in degrees
-    # ra_min, ra_max = ra - args.window, ra + args.window
-    # dec_min, dec_max = dec - args.window, dec + args.window
-    
-    # OpSim query
-    ops_visit = opsim_util.get_opsim_visit(ra, dec, args.db)
-    ops_header = opsim_util.assemble_instcat_header(ops_visit,
-                                                        seed=args.seed,
-                                                        exptime_=args.exptime,
-                                                        filter_=args.filter)
-    expid = "00"+str(int(ops_header["obshistid"]))
-    
-    # TRILEGAL query
-    out = query_data_lab(healpix, args.N)  
-    
-    save_summary(expid, out, ops_header, args)
-    if not args.noplot:
-        import matplotlib.pyplot as plt
-        import plotting
-        plt.style.use(args.mplstyle)
-        
-        plotting.plot_stars(out, ops_header)
-        
-    make_inst_cat(out, header=ops_header, catdir=args.catdir, dust=args.dust)
+# def main(args):
+#     import opsim_util
+#     ra, dec = opsim_util.get_celestial_coord(args.l, args.b)
+
+#     import healpy
+#     healpix = healpy.ang2pix(256, theta=args.l, phi=args.b, lonlat=True)
+
+#     # OpSim query
+#     ops_visit = opsim_util.get_opsim_visit(ra, dec, args.db)
+#     ops_header = opsim_util.assemble_instcat_header(ops_visit,
+#                                                         seed=args.seed,
+#                                                         exptime_=args.exptime,
+#                                                         filter_=args.filter)
+#     expid = "00"+str(int(ops_header["obshistid"]))
+
+#     # TRILEGAL query
+#     out = query_data_lab(healpix, args.N)
+
+#     save_summary(expid, out, ops_header, args)
+#     if not args.noplot:
+#         import matplotlib.pyplot as plt
+#         import plotting
+#         plt.style.use(args.mplstyle)
+
+#         plotting.plot_stars(out, ops_header)
+
+#     make_inst_cat(out, header=ops_header, catdir=args.catdir, dust=args.dust)
 
 
-def get_args():
-    from argparse import ArgumentParser
-    parser = ArgumentParser()
-    parser.add_argument('--login', default=False, action='store_true')
-    parser.add_argument('--l', default=0, type=float)
-    parser.add_argument('--b', default=0, type=float)
-    parser.add_argument('--seed', default=23526, type=int)
-    parser.add_argument('--exptime', default=15., type=float)
-    parser.add_argument('--filter', default=2, type=int)
-    parser.add_argument('--N', default=100000, type=int)
-    parser.add_argument('--window', default=0.12, type=float)
-    parser.add_argument('--dust', default=True, action='store_false')
-    parser.add_argument('--noplot', default=False, action='store_true')
-    parser.add_argument('--catdir', default='./instcats')
-    parser.add_argument('--db', default='~/Documents/share/sim_basline/baseline_v3.3_10yrs.db')
-    parser.add_argument('--mplstyle', default='~/Documents/clem.mplstyle')
-    args = parser.parse_args()
-    return args
-    
-if __name__ == '__main__':
-    
-    args = get_args()
-    
-    if args.login:
-        from getpass import getpass
-        from dl import authClient as ac
-        token = ac.login(input("Enter user name: "),getpass("Enter password: "))
-        ac.whoAmI()
-        
-    main(args)
+# def get_args():
+#     from argparse import ArgumentParser
+#     parser = ArgumentParser()
+#     parser.add_argument('--login', default=False, action='store_true')
+#     parser.add_argument('--l', default=0, type=float)
+#     parser.add_argument('--b', default=0, type=float)
+#     parser.add_argument('--seed', default=23526, type=int)
+#     parser.add_argument('--exptime', default=15., type=float)
+#     parser.add_argument('--filter', default=2, type=int)
+#     parser.add_argument('--N', default=100000, type=int)
+#     parser.add_argument('--window', default=0.12, type=float)
+#     parser.add_argument('--dust', default=True, action='store_false')
+#     parser.add_argument('--noplot', default=False, action='store_true')
+#     parser.add_argument('--catdir', default='./instcats')
+#     parser.add_argument('--db', default='~/Documents/share/sim_basline/baseline_v3.3_10yrs.db')
+#     parser.add_argument('--mplstyle', default='~/Documents/clem.mplstyle')
+#     args = parser.parse_args()
+#     return args
+
+# if __name__ == '__main__':
+
+#     args = get_args()
+
+#     if args.login:
+#         from getpass import getpass
+#         from dl import authClient as ac
+#         token = ac.login(input("Enter user name: "),getpass("Enter password: "))
+#         ac.whoAmI()
+
+#     main(args)
